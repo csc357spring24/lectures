872af4e1b2dd35cd278fc5f36fded285a7a63a59
Christopher Siu <christopher@siu.sh>
2024-04-10 14:01:09 -0700

    Added examples of pointers.

--- 04.10-pointers/swap.c
+++ 04.10-pointers/swap.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-void swap(int, int);
+void swap(int *, int *);
 
 int main(void) {
     int z = 1, w = 2;
 
-    swap(z, w);
+    /* Pointers allow us to tell a function *where* to find a value, rather
+     *  than *what* that value is, so that the function has access to the same
+     *  values that we do: */
+    swap(&z, &w);
 
-    printf("z: %d\n", z);
-    printf("w: %d\n", w);
+    printf("z (%p): %d\n", (void *)&z, z);
+    printf("w (%p): %d\n", (void *)&w, w);
 
     return 0;
 }
 
-void swap(int x, int y) {
-    int temp = x;
-    x = y;
-    y = temp;
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+
+    printf("x (%p): %p\n", (void *)&x, (void *)x);
+    printf("y (%p): %p\n", (void *)&y, (void *)y);
 }
