fe3451bcbd239da9803471a51a0fd114af4352a4
Christopher Siu <christopher@siu.sh>
2024-04-12 14:37:59 -0700

    Added examples of arrays as pointers.

--- 04.12-arrays/reverse.c
+++ 04.12-arrays/reverse.c
@@ -4,12 +4,21 @@ void swap(int *, int *);
 void reverse(int[], int);
 
 int main(void) {
-    int z = 1, w = 2;
+    int arr[4] = {1, 2, 3, 4};
 
-    swap(&z, &w);
+    /* Arrays are just addresses of contiguous blocks of memory; they have no
+     *  length attribute, and the length must be passed separately if needed: */
+    reverse(arr, 4);
 
-    printf("z (%p): %d\n", (void *)&z, z);
-    printf("w (%p): %d\n", (void *)&w, w);
+    /* Since the value of an array is a reference to its first element, any
+     *  modifications to the array within a function will be reflected outside
+     *  that function: */
+    
+    printf("arr: %p\n", (void *)arr);
+    printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
+    printf(" |- %p: %d\n", (void *)&arr[1], arr[1]);
+    printf(" |- %p: %d\n", (void *)&arr[2], arr[2]);
+    printf(" +- %p: %d\n", (void *)&arr[3], arr[3]);
 
     return 0;
 }
@@ -24,9 +33,12 @@ void swap(int *x, int *y) {
 }
 
 void reverse(int arr[], int len) {
-    printf("arr (%p): %p\n", (void *)&arr, (void *)arr);
-    printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
-    printf(" |- %p: %d\n", (void *)&arr[1], arr[1]);
-    printf(" |- %p: %d\n", (void *)&arr[2], arr[2]);
-    printf(" +- %p: %d\n", (void *)&arr[3], arr[3]);
+    /* Here, "arr" is secretly a pointer, both pointers and arrays are just
+     *  addresses, and the compiler will let us use pointer and array syntax
+     *  interchangeably: */
+    int i;
+
+    for (i = 0; i < len / 2; i++) {
+        swap(&arr[i], &arr[len - 1 - i]);
+    }
 }
