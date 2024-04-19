8f65185baaa928a2f4a0591694451c125f48e8f6
Christopher Siu <christopher@siu.sh>
2024-04-19 13:49:02 -0700

    Added basic examples of dynamic allocation.

--- 04.19-malloc/pair.c
+++ 04.19-malloc/pair.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int *pair(int, int);
@@ -11,14 +12,30 @@ int main(void) {
     printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
     printf(" +- %p: %d\n", (void *)&arr[1], arr[1]);
 
+    /* Since we the programmers uniquely know what our data represents, we the
+     *  programmers uniquely know when we no longer need that data, and thus
+     *  we are responsible for deallocating it: */
+    free(arr);
+
     return 0;
 }
 
 int *pair(int first, int second) {
-    int arr[2];
+    /* Returning a pointer to a local array is unsafe, since that array ceases
+     *  to exist and its memory is repurposed after the function returns. If
+     *  this array were global, then returning its pointer would be safe, but
+     *  we would only ever be able to make one of them: */
+    int *arr = (int *)malloc(sizeof(int) * 2);
 
+    /* This is a pointer to a block of memory on the heap, not truly an array,
+     *  but array and pointer syntax is interchangeable, and the compiler will
+     *  let us pretend that it's actually an array: */
     arr[0] = first;
     arr[1] = second;
+
+    printf("arr: %p\n", (void *)arr);
+    printf(" |- %p: %d\n", (void *)&arr[0], arr[0]);
+    printf(" +- %p: %d\n", (void *)&arr[1], arr[1]);
 
     return arr;
 }
