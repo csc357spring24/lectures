ea4a88001a95ac37329c84b8cac1d46f2f7405f9
cesiu <cesiu@calpoly.edu>
2024-04-24 12:11:58 -0700

    Added example of linked list implementation,

--- 04.24-linked-lists/list.c
+++ 04.24-linked-lists/list.c
@@ -3,12 +3,35 @@
 
 /* lstcreate: Creates an empty linked list. */
 List *lstcreate() {
-    return NULL;
+    List *lst = (List *)malloc(sizeof(List));
+
+    lst->head = NULL;
+    lst->size = 0;
+
+    return lst;
 }
 
 /* lstdestroy: Destroys an existing linked list. */
 void lstdestroy(List *lst) {
-    return;
+
+    /* NOTE: Freeing the List only frees the List; it does not free any of the
+     *       Nodes contained within. Additionally, the Nodes have to be freed
+     *       (or we have to save a pointer to the head) before we free the List
+     *       otherwise we'll lose the pointer that enables us to access them. */
+
+    Node *node = lst->head;
+
+    while (node != NULL) {
+        Node *next = node->next;
+        free(node);
+        node = next;
+    }
+
+    /* NOTE: There is no need to set any of the head or next pointers back to
+     *       NULL; the list is being deallocated, so we shouldn't need to care
+     *       what its memory contains. */
+
+    free(lst);
 }
 
 /* lstget: Gets an element in a linked list. */
@@ -23,6 +46,30 @@ int lstset(List *lst, int idx, void *val) {
 
 /* lstadd: Adds an element to a linked list. */
 int lstadd(List *lst, int idx, void *val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->val = val;
+    node->next = NULL;
+
+    if (idx == 0) {
+        node->next = lst->head;
+        lst->head = node;
+    }
+    else {
+        Node *prev = lst->head;
+        int i;
+
+        for (i = 0; i < idx - 1; i++) {
+            prev = prev->next;
+        }
+
+        node->next = prev->next;
+        prev->next = node;
+    }
+
+    lst->size++;
+
+    /* If we wanted to check for, for example, invalid indices, we could then
+     *  return something other than 0 to indicate an error. */
     return 0;
 }
 
