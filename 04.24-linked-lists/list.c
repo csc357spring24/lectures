69121eb868a526f76c636ffed49fc3126097ae89
cesiu <cesiu@calpoly.edu>
2024-04-24 13:58:28 -0700

    Added example of linked lists.

--- 04.24-linked-lists/list.c
+++ 04.24-linked-lists/list.c
@@ -3,12 +3,30 @@
 
 /* lstcreate: Creates an empty linked list. */
 List *lstcreate() {
-    return NULL;
+    List *lst = (List *)malloc(sizeof(List));
+
+    /* Memory dynamically allocated with malloc is not automatically zeroed
+     *  out; if we want zeroes there, we should put zeroes there: */
+
+    lst->head = NULL;
+    lst->size = 0;
+
+    return lst;
 }
 
 /* lstdestroy: Destroys an existing linked list. */
 void lstdestroy(List *lst) {
-    return;
+    Node *node = lst->head;
+
+    while (node != NULL) {
+        Node *next = node->next;
+        free(node);
+        node = next;
+    }
+
+    /* TODO: This only frees the List, not its Nodes... */
+
+    free(lst);
 }
 
 /* lstget: Gets an element in a linked list. */
@@ -23,6 +41,31 @@ int lstset(List *lst, int idx, void *val) {
 
 /* lstadd: Adds an element to a linked list. */
 int lstadd(List *lst, int idx, void *val) {
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->val = val;
+    node->next = NULL;
+
+    if (idx == 0) {
+        node->next = lst->head;
+        lst->head = node;
+    }
+    else {
+        Node *prev = lst->head;
+        int i;
+
+        for (i = 0; i < idx - 1; i++) {
+            prev = prev->next;
+        }
+
+        node->next = prev->next;
+        prev->next = node;
+    }
+
+    lst->size++;
+
+    /* If we cared to check for indexing out of bounds, we could do that and
+     *  then potentially return a non-zero error code. */
+
     return 0;
 }
 
