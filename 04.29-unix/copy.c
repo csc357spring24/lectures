809abe36127a9578a69e0e439e05a1b5dada27c8
cesiu <cesiu@calpoly.edu>
2024-04-29 13:58:57 -0700

    Added examples of system calls.

--- 04.29-unix/copy.c
+++ 04.29-unix/copy.c
@@ -1,21 +1,32 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-#define SIZE 16
+/* Increasing the size of the buffer does not reduce the total bytes read or
+ *  written, but it does reduce the total system calls needed to read or write
+ *  all of those bytes. A system call is more expensive than an ordinary
+ *  function call -- there is additional overhead associated with transferring
+ *  control to and from the OS, and it is in our best interests to limit the
+ *  total number of system calls. */
+#define SIZE 4096
 
 int main(int argc, char *argv[]) {
     char buf[SIZE];
     int n;
-    FILE *src, *dest;
+    
+    /* The system call open returns an integer file descriptor rather than a
+     *  pointer to a FILE structure -- this is the bare minimum information
+     *  needed to interact with an open file. */
+    int src, dest;
 
-    src = fopen(argv[1], "r");
-    dest = fopen(argv[2], "w");
+    src = open(argv[1], O_RDONLY);
+    dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-    while ((n = fread(buf, sizeof(char), SIZE, src)) > 0) {
-        fwrite(buf, sizeof(char), n, dest);
+    while ((n = read(src, buf, sizeof(char) * SIZE)) > 0) {
+        write(dest, buf, sizeof(char) * n);
     }
 
-    fclose(src);
-    fclose(dest);
+    close(src);
+    close(dest);
 
     return 0;
 }
