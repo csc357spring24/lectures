f66b7b00d7141075f56128a43a69abf3eed0cccf
cesiu <cesiu@calpoly.edu>
2024-05-01 13:57:35 -0700

    Added examples of bitpacking and error checking.

--- 05.01-bitpacking/pack.c
+++ 05.01-bitpacking/pack.c
@@ -10,10 +10,26 @@ int main(int argc, char* argv[]) {
     char buf[8];
     int src, dest, n;
 
-    src = open(argv[1], O_RDONLY);
-    dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    /* There are no exceptions in C; instead, system calls will set a global
+     *  variable errno to indicate any errors. The standard library function
+     *  perror can be used to print human-readable error messages to stderr
+     *  based on that variable. */
+    if ((src = open(argv[1], O_RDONLY)) < 0) {
+        perror(argv[1]);
+        exit(EXIT_FAILURE);
+    }
 
+    if ((dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644)) < 0) {
+        perror(argv[2]);
+        close(src);
+        exit(EXIT_FAILURE);
+    }
+
+    /* The standard library and the system calls cannot read or write fewer
+     *  than 1 byte at a time. We'll have to read in 8 "bits" at once, then
+     *  pack them into a single byte: */
     while ((n = read(src, buf, sizeof(char) * 8)) > 0) {
+        /* ...which means the last byte may need to be padded with 0's: */
         for (; n < 8; n++) {
             buf[n] = '0';
         }
@@ -25,9 +41,20 @@ int main(int argc, char* argv[]) {
     close(src);
     close(dest);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 unsigned char stob(char *bits) {
-    return '\0';
+    unsigned char byte = 0, mask;
+
+    /* Note that the string will be of the form "00100001", with the character
+     *  at index 0 being the MSB of the byte we're packing. */
+
+    for (mask = 0x80; mask > 0; mask >>= 1) {
+        if (*(bits++) == '1') {
+            byte = byte | mask;
+        }
+    }
+
+    return byte;
 }
