05e1593c18d9e90245ae7a9c33750d115b35ed8a
cesiu <cesiu@calpoly.edu>
2024-05-13 13:58:30 -0700

    Added example of directories as files.

--- 05.13-directories/list.c
+++ 05.13-directories/list.c
@@ -5,15 +5,37 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    DIR *dir;
+    struct dirent *entry;
     struct stat buf;
 
-    if (stat(argv[1], &buf) < 0) {
-        perror("stat");
-        exit(EXIT_FAILURE);
+    /* Every process has a current working directory, relative to which all
+     *  file system operations are performed: */
+
+    chdir(argv[1]);
+    dir = opendir(".");
+
+    while ((entry = readdir(dir)) != NULL) {
+        
+        /* Each directory entry is only guaranteed to contain a filename and
+         *  the number of the inode to which it is mapped -- any additional
+         *  information must be gathered by calling stat: */
+
+        if (stat(entry->d_name, &buf) < 0) {
+            perror("stat");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("\"%s\" -> %ld (%ld bytes in %ld blocks)\n",
+         entry->d_name, (long)(buf.st_ino), (long)(buf.st_size), (long)(buf.st_blocks));
+
+        /* Note that a directory entry is only guaranteed to persist until the
+         *  directory is closed or until readdir is called again. If we want to
+         *  keep any of its information around for later, we have to make a
+         *  copy ourselves. */
     }
 
-    printf("\"%s\" -> %ld (%ld bytes in %ld blocks)\n",
-     argv[1], (long)(buf.st_ino), (long)(buf.st_size), (long)(buf.st_blocks));
+    closedir(dir);
 
     return EXIT_SUCCESS;
 }
